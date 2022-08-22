02d4b5ea4779c7d53e3fa793c31761f2a08238e8
manishrai0021 <94151781+manishrai0021@users.noreply.github.com>
2022-08-22 11:48:52 +0545

    New Banner

--- src/App.js
+++ src/App.js
@@ -70,7 +70,6 @@ export const App = () => {
         <Header handler={menuHandler} isMenuOpen={isMenuOpen} />
         {/* <NavbarBottom /> */}
         <MobileMenu isMenuOpen={isMenuOpen} />
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-  
+    extend: {
+      animation: {
+        run: "run 1s linear",
+      },
+      keyframes: {
+        run: {
+          "0%": { right: "90%" },
+          "50%": { right: "50%" },
+          "100%": { right: "20%" },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
